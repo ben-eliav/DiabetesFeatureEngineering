848b9165a3429611f03537c4e912e9d0f39cac0f
ben-eliav <eliav123.spam@gmail.com>
2023-12-12 15:57:37 +0200

    Bug fixes and adding loss to validation

--- train_eval.py
+++ train_eval.py
@@ -45,19 +45,25 @@ class Trainer:
         self.model.eval()
         out = self.model(self.data.x_dict, self.data.edge_index_dict)[subpop]
         mask = self.data[subpop].val_mask if val else self.data[subpop].test_mask
+        sse = (out[mask] - self.data[subpop].y[mask]).pow(2).sum().item()
         return ndcg_score(self.data[subpop].y[mask].detach().numpy().reshape(1, -1),
-                          out[mask].detach().numpy().reshape(1, -1))
+                          out[mask].detach().numpy().reshape(1, -1)), sse
 
-    def test(self, val=True):
+    def test(self, validation=True):
         """
         Testing each layer separately
 
-        :param val: True if validation, False if test
-        :return: SSE for each layer, total SSE
+        :param validation: True if validation, False if test
+        :return: NDCG values for each subpop, sum of NDCG values, sse loss
         """
         self.model.eval()
-        ndcg = {key: self.test_subpop(key, val) for key in self.data.x_dict}
-        return ndcg, sum(ndcg.values())
+        ndcg = {}
+        sse = 0
+        for key in self.data.x_dict:
+            ndcg_subpop, sse_subpop= self.test_subpop(key, validation)
+            ndcg[key] = ndcg_subpop
+            sse += sse_subpop
+        return ndcg, sum(ndcg.values()), sse
 
     def baseline_prediction(self):
         """
@@ -73,17 +79,17 @@ class Trainer:
             predictions.append(sum(labels) / len(labels))
         return torch.tensor(predictions, dtype=torch.float32)
 
-    def baseline_ndcg(self, val=True):
+    def baseline_ndcg(self, validation=True):
         """
         Calculate NDCG for baseline prediction
 
-        :param val: True if validation, False if test
+        :param validation: True if validation, False if test
         """
         predictions = torch.tensor(self.baseline_prediction())
 
         ndcg = {}
         for key in self.data.x_dict:
-            mask = self.data[key].val_mask if val else self.data[key].test_mask
+            mask = self.data[key].val_mask if validation else self.data[key].test_mask
             ndcg[key] = ndcg_score(self.data[key].y[mask].detach().numpy().reshape(1, -1),
                                    predictions[mask].numpy().reshape(1, -1))
         return ndcg, sum(ndcg.values())
@@ -95,20 +101,12 @@ class Trainer:
         for epoch in range(EPOCHS):
             _, loss, train_ndcg = self.train()
             loss_epochs.append(loss)
-            val_ndcg = self.test()[1]
+            val_ndcg_dict, val_ndcg, val_sse = self.test()
             val_ndcg_epochs.append(val_ndcg)
-            print(f'Epoch: {epoch}, Loss: {loss}, Val NDCG: {val_ndcg}')
+            print(f'Epoch: {epoch}, Train Loss: {loss}, Val Loss: {val_sse}')
             print(f'Train NDCG: {train_ndcg}')
+            print(f'Val NDCG: {val_ndcg_dict}')
             if best_ndcg < val_ndcg:
                 best_ndcg = val_ndcg
                 torch.save(self.model.state_dict(), f'Models/{self.name}.pt')
         visualization.plot_loss_ndcg(loss_epochs, val_ndcg_epochs, self.baseline_ndcg()[1])
-
-    def attempt(self):
-        self.model.eval()
-        out = self.model(self.data.x_dict, self.data.edge_index_dict)
-
-        for subpop in self.data.x_dict:
-            print(subpop)
-            loss = self.criterion(out[subpop], self.data[subpop].y)
-            print(loss)
