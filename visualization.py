1bbdf508d866cc1a72a0872e78df6b80572fd8d5
ben-eliav <eliav123.spam@gmail.com>
2023-12-14 00:55:24 +0200

    minor changes to visualization|

--- visualization.py
+++ visualization.py
@@ -3,16 +3,17 @@ from constants import *
 
 
 def plot_loss_ndcg(losses, ndcgs, baseline=None, log=True):
-    plt.plot(list(range(EPOCHS)), losses[0], label='Training loss')
-    plt.plot(list(range(EPOCHS)), losses[1], label='Validation loss')
+    plot = plt.semilogy if log else plt.plot
+    plot(list(range(EPOCHS)), losses[0], label='Training loss')
+    plot(list(range(EPOCHS)), losses[1], label='Validation loss')
     plt.legend()
     plt.title('Loss per epoch')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.show()
-    plot = plt.semilogy if log else plt.plot
-    plot(list(range(EPOCHS)), ndcgs[0], label='Training NDCG')
-    plot(list(range(EPOCHS)), ndcgs[1], label='Validation NDCG')
+
+    plt.plot(list(range(EPOCHS)), ndcgs[0], label='Training NDCG')
+    plt.plot(list(range(EPOCHS)), ndcgs[1], label='Validation NDCG')
     if baseline is not None:
         plt.axhline(y=baseline, color='r', linestyle='-', linewidth=0.5, label='Baseline NDCG')
     plt.legend()
